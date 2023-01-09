fac3a31f5475cd48b9f814d7f951d424380ac076
Noppakun Anantakitthawon <67267679+pro10150@users.noreply.github.com>
2023-01-09 17:16:46 +0700

    get movieInfo

--- api/DBManager.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("../muvi_database")
-
-c = conn.cursor()
-
-def getMovie(movieId: int):
-    c.execute("SELECT * FROM movies WHERE movie_id IS {movieId}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+import DBManager as db
 
 #init app
 app = FastAPI()
@@ -14,3 +15,9 @@ def read_root():
 @app.get("/item/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/movie/{movieId}")
+async def get_movie(movieId: int):
+    movie = await db.getMovie(movieId=movieId)
+    # return {"movie": movie}
+    return {"id": movie[0], "adult": movie[1], "belongs_to_collection": movie[2], "budget": movie[3], "homepage": movie[4], "movie_id": movie[5], "imdb_id": movie[6], "original_language": movie[7], "original_title": movie[8], "overview": movie[9], "popularity": movie[10], "poster_path": movie[11], "release_date": movie[12], "revenue": movie[13], "runtime": movie[14], "status": movie[15], "tagline" : movie[16], "title": movie[17], "vote_average": movie[18], "vote_count": movie[19]}
